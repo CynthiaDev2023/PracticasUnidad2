df60a145d543ac1929a9cdf11f08bc86ecb1b910
CynthiaDev2023 <cynthiadevtest@gmail.com>
2023-03-14 07:50:02 -0700

    se agrega un mensaje

--- EJEMPLOGIT/src/ejemplogit/EJEMPLOGIT.java
+++ EJEMPLOGIT/src/ejemplogit/EJEMPLOGIT.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package ejemplogit;
 
-/**
- *
- * @author 134
- */
+package ejemplogit;
 public class EJEMPLOGIT {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola");
     }
     
 }
